0721ff2fbec5e71c1102148e17888bc029f7baf2
Renee Radix <renee.radix@gmail.com>
2025-02-19 12:30:07 -0800

    tried to add collision detection

--- sketch.js
+++ sketch.js
@@ -15,12 +15,16 @@ function draw() {
   //emitter.addParticle();
   //emitter.run();
 
+  let gravity = createVector(0, 0.1);
+  emitter.applyGravity(gravity);
+
+
   if(mouseIsPressed == true){
     emitter.addParticle(mouseX, mouseY);
   }
   emitter.run();
+  
 
-  console.log(mouseIsPressed);
 
   colorMode(HSB);
   fill(pColor, 100, 100);
@@ -54,42 +58,57 @@ function windowResized() {
 }
 
 class Particle {
-  constructor(x,y, fill) {
+  constructor(x, y, fill) {
+    this.size = 3;
     this.position =  createVector(x, y);
-    // For demonstration purposes, the particle has a random velocity.
-    // Starts by moving up a little bit and in a random left or right direction slightly
-    this.velocity = createVector(random(-1, 1), random(-1, 0));
+    this.velocity = createVector(random(-.5, .5), random(-.5, 0));
     this.acceleration = createVector(0, 0);
     this.lifespan = 255.0;
     this.fill = fill;
-    this.fillFact = map(fill, 0, 360, 0.001, 0.05);
+    this.mass = map(fill, 0, 360, 1, 5);
+    this.stopped = false;
   }
 
 
   run() {
     // Adds acceleration to velocity, adding velocity to the location
     // Then acceleration gets set to 0 because the force is getting re-added to it every draw loop
-    let gravity = createVector(0, this.fillFact); // gravity needs to be declared as a variable to use the createVector function, since passing applyForce two numbers won't make it register a vector
-      if (this.position.y < height - 10){
-      this.applyForce(gravity);
-      this.velocity.add(this.acceleration);
-      this.position.add(this.velocity);
-      //this.lifespan -= 2.0;
-      this.acceleration.mult(0);
+    let gravity = createVector(0, 0.05); // gravity needs to be declared as a variable to use the createVector function, since passing applyForce two numbers won't make it register a vector
+
+      if (this.stopped == false){
+        this.velocity.add(this.acceleration);
+        this.position.add(this.velocity);
       }
-      if (this.position.y >= height - 10){
-        this.velocity.mult(1, 0);
+    
+      this.acceleration.mult(0);
+      if (this.position.y >= height - 10 || this.stopped == true){
+        //mess with this if you want to make more advanced physics, currently the sand just stops immediately
+        this.velocity.mult(0);
+        this.stopped == true;
       }
     // lifespan determines the stroke and fill alpha of the particle
     noStroke();
     colorMode(HSB);
     fill(this.fill, 100, 100, this.lifespan);
-    rect(this.position.x, this.position.y, 2, 2);
+    rect(this.position.x, this.position.y, this.size, this.size);
+  }
+
+  look(theseParticles){
+    let desiredSeparation = this.size * 3;
+    for(let i = 0; i < theseParticles.length; i++){
+      const d = p5.Vector.dist(theseParticles[i].position, this.position);
+      if (this != theseParticles[i] && theseParticles[i].stopped == true && d < desiredSeparation){
+        this.stopped = true;
+      }
+    }
+
   }
 
   // Adds whatever force is being applied to it in the draw loop to the acceleration
   applyForce(force) {
-    this.acceleration.add(force);
+    let f = force.copy();
+    f.div(this.mass);
+    this.acceleration.add(f);
   }
 
   // Is the particle alive or dead?
@@ -107,13 +126,29 @@ class Emitter { //constructing an emitter class to clean up the draw loop and al
 
   addParticle(x, y){ 
       this.particles.push(new Particle(x, y, pColor));
-    
+  }
+
+  applyForce(force) {
+    for(let particle of this.particles){
+      particle.applyForce(force);
+    }
+  }
+
+  applyGravity(force) {
+    for(let i = 0; i < this.particles.length; i++){
+      if(this.particles[i].position.y <= height - 10){
+        this.particles[i].applyForce(force);
+      }
+    }
   }
 
   run(){
      //i.e. the size of the for loop is the size of the particle array this emitter has
     for (let i = this.particles.length - 1; i >= 0; i--){
       this.particles[i].run();
+      this.particles[i].look(this.particles);
+      
+      
       if (this.particles[i].isDead()) { //i.e. if particle.isDead is true
       this.particles.splice(i, 1);  // removes particle at index i
       }
